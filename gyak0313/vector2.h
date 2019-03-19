02e4af705a6ddec95bfcd832b0d18ab31244285a
onifizirib <48030636+onifizirib@users.noreply.github.com>
2019-03-19 20:22:52 +0100

    Update vector2.h

--- gyak0313/vector2.h
+++ gyak0313/vector2.h
@@ -7,22 +7,15 @@ T sq(T const&a)
 {
     return a*a;
 }
-
+//------------------------------------------
 
 template<typename T>
 struct Vector2
 {
-    T x, y;
-     
-     //skalárral szorzás... valamelyik oldalról..
-    Vector2<T> operator*(T const& p)
-    {
-        return Vector2{x*p, y*p};
-        //nem kell this->x, elég x
-    }
-    //TODO: másik oldalról szorzás
+    T x, y;  
 
-     //skalárral osztás
+
+     //skalárral osztás (mivel azt úgyis csak egyik oldalról lehet)
      Vector2<T> operator/(T const& p)
      {
          if ((T)0 == p)
@@ -34,6 +27,7 @@ struct Vector2
              std::cout << "dividing with zero!" << std::endl;
          }
          return Vector2{x/p, y/p};
+         //nem kell this->x, elég x
      }
 
     //hossznégyzet
@@ -58,18 +52,78 @@ struct Vector2
         return Vector2{x/len(this), y/len(this)};
      }
 
-     //elforgatás
+     //elforgatás egy radiánban adott szöggel
+     Vector2<T> rotate(T const& angle)
+     {
+         /*
+         T típusú a szög, az egyszerűség kedvéért, hogy ne kelljen a vektor típusán is változtatni
+         persze ha kívülre raknám, akkor valószínűleg elég lenne egy másféle template-s vektorral visszatérni
+         */
+         return Vector2{x*cos(angle)-y*sin(angle), x*sin(angle)+y*cos(angle)};
+     }
 
      //+= operátor
-     
+     Vector2<T>& operator+=(Vector2<T> const& v)
+    {
+        x += v.x; 
+        y += v.y;
+        return *this;
+    }
+    
+    //-= operátor
+    Vector2<T>& operator-=(Vector2<T> const& v)
+    {
+        x -= v.x; 
+        y -= v.y;
+        return *this;
+    }
+    
+    //*= operátor csak skalárral való szorzásra lesz értelmezve, megkülönböztetve a dot-tól
+    Vector2<T>& operator*=(T const& p)
+    {
+        x *= p; 
+        y *= p;
+        return *this;
+    }
+    
+    // /= operátor csak skalárral való jobbról osztásra lesz értelmezve
+    Vector2<T>& operator/=(T const& p)
+    {
+        x /= p; 
+        y /= p;
+        return *this;
+    }    
 
 };
+
+//----------------------------------------------
+
+//skalárral szorzás bal oldalról
+template<typename T>
+Vector2<T> operator*(T const& p, Vector2<T>& v)
+{
+    return Vector2<T>{v.x*p, v.y*p};
+}
+
+//skalárral szorzás jobb oldalról
+template<typename T>
+Vector2<T> operator*(Vector2<T>& v, T const& p)
+{
+    return Vector2<T>{v.x*p, v.y*p};
+}
 
 //összeadás
 template<typename T>
 Vector2<T> operator+(Vector2<T> const& a, Vector2<T> const& b)
 {
     return Vector2<T>{a.x + b.x, a.y + b.y};
+}
+
+//kivonás
+template<typename T>
+Vector2<T> operator-(Vector2<T> const& a, Vector2<T> const& b)
+{
+    return Vector2<T>{a.x - b.x, a.y - b.y};
 }
 
 //skalárszorzat
@@ -93,6 +147,16 @@ T sqlen(Vector2<T> const& v)
 {
     return sq(v.x)+sq(v.y);
     //v.x^2 bitenként xor-ozza a kettővel. írjunk külön függvényt
+}
+
+template<typename T>
+Vector2<T> norm(Vector2<T> const& v)
+{
+    if (len(v) == (T)0)
+    {
+        std::cout << "vector with zero length is being normalised!" << std::endl;
+    }
+    return Vector2<T>{v.x/len(v), v.y/len(v)};
 }
 
 //kiiratás << operátorral
