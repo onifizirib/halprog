7beddb645422152077cbbae6c441ad98b7557b96
onifizirib <48030636+onifizirib@users.noreply.github.com>
2019-04-23 02:40:23 +0200

    Update matrix.h

--- hf0320/matrix.h
+++ hf0320/matrix.h
@@ -1,4 +1,4 @@
-// legtöbb hibakezelés hiányos
+// van kezdetleges hibadobálás a méretkülönbségekre
 
 #include <vector>
 #include <algorithm>
@@ -11,6 +11,28 @@
 inline auto add = [](auto const& x, auto const& y){return x+y;};
 inline auto sub = [](auto const& x, auto const& y){return x-y;};
 
+//segéd struct-ok, hogy elkülöníthető legyenek az ezek nélkül azonos szintaxisú konstruktorok
+struct lambda1{};
+struct lambda2{};
+struct specimen1{};
+/*
+a specimen itt arra utal, hogy pl egy vektor egy példányát lehet átadni -> matrix data-ját azon vektor elemei fogják alkotni
+le kell ellenőrizni, hogy a vektor példány mellé megadott sizeN négyzete megegyezik-e a vektor hosszával!
+*/
+
+//hibahajigáláshoz
+auto err = [](auto str){ std::cout << "MatrixNxN.h error: " << str << "\n"; std::exit(-1); };
+auto sizerr() 
+{
+    err("matrices do not have the same size!");
+    /*
+    Q: kell-e itt visszatérni bármivel is? eredetileg void függvénnyel próbálkoztam és az err(..) után állt egy return;
+    de kaptam egy warningot, miszerint: control reaches end of non-void function [-Wreturn-type]
+    így átírtam auto visszatérési értékűre. de ugyanazt a warningot kapom.
+    */
+}
+
+//------------------------------------------------------------------------------------------------------------------------
 template<typename T>
 struct MatrixNxN
 {
@@ -27,8 +49,7 @@ struct MatrixNxN
     MatrixNxN()
     {
         /* 
-        Q: csak simán vector<T> data; ? tudni fogja, hogy az a data az a matrixNxN data-jára vonatkozik?
-        A: vector<T> data -> neki van default const-ja -> nem kell itt írni
+        vector<T> data -> neki van default const-ja -> nem kell itt írni
         */
         sizeN = 0;
     }
@@ -39,8 +60,40 @@ struct MatrixNxN
 		sizeN = 0;
 		std::swap(sizeN, mv.sizeN);		
 	}
-
-    //konstruktor megadott elemekből
+    
+    template<typename F>
+    MatrixNxN(lambda1, int n, F f)
+    {
+        sizeN = n;
+        n *= n;
+        data.resize(n);
+        for(int i=0; i<n; i++) {data[i] = f(i);}
+    }
+    
+    template<typename F>
+    MatrixNxN(lambda2, int n, F f)
+    {
+        sizeN = n;
+        data.resize(n*n); 
+        for(int i=0; i<n; i++)
+        {
+            for(int j=0; j<n; j++)
+            {
+                data[i*n+j] = f(i,j);
+            }
+        }
+    }
+    
+    template<typename F>
+    MatrixNxN(specimen1, int n, F f)
+    {
+        sizeN = n;
+        n *= n;
+        data.resize(n);
+        for(int i=0; i<n; i++) {data[i] = f[i];}
+    }    
+    
+    //konstruktor függvény szerint megadott elemekből
     template<typename F>
     MatrixNxN(int n, F f)
     {
@@ -50,9 +103,9 @@ struct MatrixNxN
         data.resize(n);        
         for(int i=0; i<n; i++) {data[i] = f(i);}
         /*
-        Q: f(i) nem működne std::vector-ra, ezért írtam f[i]-t.
-        A: eredetileg függvény lenne f, pl egy lambda -> gömbölyűvel indexelés
-        így visszatértem az f(i) formátumra
+        Q: ha a felhasználó iyen formában ad meg az F helyén egy lambdával vagy egy vektorral, azt hogyan kéne megkülönböztetni?
+        feljebb megírtam az egy és két indexes lambdákhoz a konstruktort, meg a vektorra jó jonstruktort, de hogyan döntöm el itt,
+        hogy melyiket kell meghívni?
         */
     }
 
@@ -75,40 +128,45 @@ struct MatrixNxN
 		mv.sizeN = (T)0;
         return *this;
     }
-    /*
-    Q: destruktort kell írnom? az világos, hogy minden kostruktort meg kellett ha akár egyet is írtam. ha igen, hogyan?
-    A: nem szükséges destruktort írni.
-    */
 
-    //műveletekhez lehessen használni vektorszerűként (csak olvasó esetekre cbegin cend)
-    /*
-    Q: ez így oké? nem kontárkodok így bele abba, ahogy eltárolja magát egy ilyen matrix struct egy példánya? pl nem tűnik-e/mutat-e még valami el a sizeN tag(ra)?
-    A: nem lesz gond, nem akad össze semmivel
-    */
     
     auto begin()   		{return data.begin();}
     auto cbegin() const {return data.cbegin();}
     auto end()      	{return data.end();}
     auto cend()   const	{return data.cend();}
+    
+    //NxN mátrix mérete (N)
+    int size() const {return sizeN;} 
+    /*
+    valamiért más szóköz/tabulátor mennyiséggel nem akart lefordulni.. úgyhogy egy sorban van.
+    */
 
     //+= operátor
     MatrixNxN<T>& operator+= (MatrixNxN<T> const& cpy)
     {
-        std::transform(*this.begin(),*this.end(),cpy.cbegin(),*this.begin(), add);
+        if (this->size() != cpy.size()) {sizerr();}
+        std::transform(this->begin(), this->end(), cpy.cbegin(), this->begin(), add);
         return *this;
+        /*
+        this: pointer to the current object
+        *this: dereferenced
+        Tehát a következő is működik (zárójelek a precedencia miatt kellenek):
+           std::transform((*this).begin(), (*this).end(), cpy.cbegin(), (*this).begin(), add);
+        */
     }
 
     //-= oerátor
     MatrixNxN<T>& operator-= (MatrixNxN<T> const& cpy)
     {
-        std::transform(*this.begin(),*this.end(),cpy.cbegin(),*this.begin(), sub);
+        if (this->size() != cpy.size()) {sizerr();}
+        std::transform(this->begin(), this->end(), cpy.cbegin(), this->begin(), sub);
         return *this;
     }
 
     //*= operátor (csak skalárral szorzásra)
     MatrixNxN<T>& operator*= (T const& scal)
     {
-        std::transform(*this.begin(),*this.end(),*this.begin(), [=](T const& x){return x*scal;});
+        std::transform(this->begin(), this->end(), this->begin(), [=](T const& x){return x*scal;});
         return *this;
     }
 
@@ -120,50 +178,38 @@ struct MatrixNxN
             std::cout << "dividing with zero" << std::endl;
             return *this;
         }
-        std::transform(*this.begin(),*this.end(),*this.begin(), [=](T const& x){return x/scal;});
+        std::transform(this->begin(), this->end(), this->begin(), [=](T const& x){return x/scal;});
         return *this;
-    }
-
-    //NxN mátrix mérete (N)
-    int size()
-    {
-        return sizeN;
     }
 };
 //-------------------------------------------------------------------------
 
 //méret-egyezést ellenőrző függvény:
-//lehetne ez bool visszatérési értékű, és ott, ahol meg van hívva, az eredménytől függően továbbhaladni. egyelőre csak kiírás történik. 
 template<typename T1, typename T2>
-void sizecheck (MatrixNxN<T1> const& m1, MatrixNxN<T2> const& m2)
+bool sizecheck (MatrixNxN<T1> const& m1, MatrixNxN<T2> const& m2)
 {
     if (m1.size() != m2.size())
     {
-        std::cout << "the two matrices do not have the same size \n";
+        return false;
     }
-    return;
+    else
+    {
+        return true;
+    }
 }
 
 //összeadás mátrixokra (4 verzió)
 template<typename T>
 MatrixNxN<T> operator+ (MatrixNxN<T> const& m1, MatrixNxN<T> const& m2)
 {
-    sizecheck(m1, m2);
-    MatrixNxN<T> result;
-    result.data.resize(m1.data.size());
-    result.sizeN = m1.sizeN;
-    std::transform(m1.cbegin(),m1.cend(),m2.cbegin(),result.begin(),add);
-    /*
-    szóval ha jól értem, a  MatrixNxN<T> result; sor ügyes átfogalmazásával és megfelelő konstruktorok írásával a return-ig a sorok megspórolhatóak
-    mikor szükséges "2 indexes konstruktort" használni? amit még nehezen tudok elképzelni, hogy hogyan kéne kinézzen
-    */
-    return result;
+    if (!sizecheck(m1, m2)) {sizerr();}
+    return MatrixNxN<T>(lambda1{}, m1.size(), [&](int i){return m1[i]+m2[i];});
 }
 
 template<typename T>
 MatrixNxN<T>&& operator+ (MatrixNxN<T>&& m1, MatrixNxN<T> const& m2)
 {
-    sizecheck(m1, m2);
+    if (!sizecheck(m1,m2)) {sizerr();}
     std::transform(m1.begin(),m1.end(),m2.cbegin(),m1.begin(),add);
     return std::move(m1);
 }
@@ -171,7 +217,7 @@ MatrixNxN<T>&& operator+ (MatrixNxN<T>&& m1, MatrixNxN<T> const& m2)
 template<typename T>
 MatrixNxN<T>&& operator+ (MatrixNxN<T> const& m1, MatrixNxN<T>&& m2)
 {
-    sizecheck(m1, m2);
+    if (!sizecheck(m1,m2)) {sizerr();}
     std::transform(m1.cbegin(),m1.cend(),m2.begin(),m2.begin(),add);
     return std::move(m2);
 }
@@ -179,7 +225,7 @@ MatrixNxN<T>&& operator+ (MatrixNxN<T> const& m1, MatrixNxN<T>&& m2)
 template<typename T>
 MatrixNxN<T>&& operator+ (MatrixNxN<T>&& m1, MatrixNxN<T>&& m2)
 {
-    sizecheck(m1, m2);
+    if (!sizecheck(m1,m2)) {sizerr();}
     std::transform(m1.begin(),m1.end(),m2.cbegin(),m1.begin(),add);
     return std::move(m1);
 }
@@ -188,18 +234,14 @@ MatrixNxN<T>&& operator+ (MatrixNxN<T>&& m1, MatrixNxN<T>&& m2)
 template<typename T>
 MatrixNxN<T> operator- (MatrixNxN<T> const& m1, MatrixNxN<T> const& m2)
 {
-    sizecheck(m1, m2);
-    MatrixNxN<T> result;
-    result.data.resize(m1.data.size());
-    result.sizeN = m1.sizeN;
-    std::transform(m1.cbegin(),m1.cend(),m2.cbegin(),result.begin(),sub);
-    return result;
+    if (!sizecheck(m1,m2)) {sizerr();}
+    return MatrixNxN<T>(lambda1{}, m1.size(), [&](int i){return m1[i]-m2[i];});
 }
 
 template<typename T>
 MatrixNxN<T>&& operator- (MatrixNxN<T>&& m1, MatrixNxN<T> const& m2)
 {
-    sizecheck(m1, m2);
+    if (!sizecheck(m1,m2)) {sizerr();}
     std::transform(m1.begin(),m1.end(),m2.cbegin(),m1.begin(),sub);
     return std::move(m1);
 }
@@ -207,7 +249,7 @@ MatrixNxN<T>&& operator- (MatrixNxN<T>&& m1, MatrixNxN<T> const& m2)
 template<typename T>
 MatrixNxN<T>&& operator- (MatrixNxN<T> const& m1, MatrixNxN<T>&& m2)
 {
-    sizecheck(m1, m2);
+    if (!sizecheck(m1,m2)) {sizerr();}
     std::transform(m1.cbegin(),m1.cend(),m2.begin(),m2.begin(),sub);
     return std::move(m2);
 }
@@ -215,7 +257,7 @@ MatrixNxN<T>&& operator- (MatrixNxN<T> const& m1, MatrixNxN<T>&& m2)
 template<typename T>
 MatrixNxN<T>&& operator- (MatrixNxN<T>&& m1, MatrixNxN<T>&& m2)
 {
-    sizecheck(m1, m2);
+    if (!sizecheck(m1,m2)) {sizerr();}
     std::transform(m1.begin(),m1.end(),m2.cbegin(),m1.begin(),sub);
     return std::move(m1);
 }
@@ -224,11 +266,7 @@ MatrixNxN<T>&& operator- (MatrixNxN<T>&& m1, MatrixNxN<T>&& m2)
 template<typename T>
 MatrixNxN<T> operator* (MatrixNxN<T> const& m, T const& scal)
 {
-    MatrixNxN<T> result;
-    result.data.resize(m.data.size());
-    result.sizeN = m.sizeN;
-    std::transform(m.cbegin(),m.cend(),result.begin(),[=](T const& x){return x*scal;});
-    return result;
+    return MatrixNxN<T>(lambda1{}, m.size(), [&](int i){return m[i]*scal;});
 }
 
 template<typename T>
@@ -241,11 +279,7 @@ MatrixNxN<T>&& operator* (MatrixNxN<T>&& m, T const& scal)
 template<typename T>
 MatrixNxN<T> operator* (T const& scal, MatrixNxN<T> const& m)
 {
-    MatrixNxN<T> result;
-    result.data.resize(m.data.size());
-    result.sizeN = m.sizeN;
-    std::transform(m.cbegin(),m.cend(),result.begin(),[=](T const& x){return scal*x;});
-    return result;
+    return MatrixNxN<T>(lambda1{}, m.size(), [&](int i){return scal*m[i];});
 }
 
 template<typename T>
@@ -260,11 +294,7 @@ template<typename T>
 MatrixNxN<T> operator/ (MatrixNxN<T> const& m, T const& scal)
 {
     if ((T)0 == scal) {std::cout << "dividing with zero \n";}
-    MatrixNxN<T> result;
-    result.data.resize(m.data.size());
-    result.sizeN = m.sizeN;
-    std::transform(m.cbegin(),m.cend(),result.begin(),[=](T const& x){return x/scal;});
-    return result;
+    return MatrixNxN<T>(lambda1{}, m.size(), [&](int i){return m[i]/scal;});
 }
 
 template<typename T>
@@ -275,73 +305,130 @@ MatrixNxN<T>&& operator/ (MatrixNxN<T>&& m, T const& scal)
     return std::move(m);
 }
 
-//mátrixok szorzása (4? verzió)
+//mátrixok szorzása (4 verzió)
 /*
     mivel az egyik mátrix esetén nem sorfolytonos az elemek kiolvasása, ezért ciklusokkal fogom implementálni a szorzást
-    UPDATE: ha módosítok elemet abban a mátrixban, amelyik részt vesz a szorzásban, az elront néhány másik szorzást is!
-    -> jobbérték referenciával nem éri meg a jelenelgi megoldás. leehtne pl létrehozni ideiglenes sort / oszlopot,
-    attól függően, hogy a bemeneti mátrixok közül melyik jobbérték. kérdés, megéri-e, elvégre egy halom pici kis másolás történne
-    ide-oda az ideiglenes egysoros tárolóból. sőt, kell akkor egyaltalán a sok egysor másolás után a move visszatéréskor??
+    jobbértékes verzióhoz létrehozni ideiglenes sort / oszlopot, attól függően, hogy a bemeneti mátrixok közül melyik jobbérték.
+	kérdés, megéri-e, elvégre egy halom pici kis másolás történne ide-oda az ideiglenes egysoros tárolóból.
 */
 template<typename T>
+//Q: érdemes-e külön template típust bevezetni a mátrixoknak? pl ha int beépített módon helyesen szorzódik egy double-lel akkor lehet értelme
 MatrixNxN<T> operator* (MatrixNxN<T> const& m1, MatrixNxN<T> const& m2)
 {
-    sizecheck(m1, m2);
-    int N = m1.size(N);
-    MatrixNxN<T> result;
-    result.data.resize(m1.data.size());
-    result.sizeN = N;
-    T tempRes = (T)0;
+    if (!sizecheck(m1,m2)) {sizerr();}
+    const int N = m1.size();
+    return MatrixNxN<T>(lambda2{}, N, [&](int i, int j)
+                                    {
+                                        T tempRes = (T)0;
+                                        for(int n=0; n<N; n++)
+                                        {
+                                            tempRes += m1(i,n)*m2(n,j);
+                                        }
+                                        return tempRes;
+                                    });
+}
+
+template<typename T>
+MatrixNxN<T>&& operator* (MatrixNxN<T>&& mv, MatrixNxN<T> const& m)
+{
+    if (!sizecheck(m,mv)) {sizerr();}
+    const int N = m.size();
+    std::vector<T> tmp;
+    //ideiglenes sor tmp
     for(int i=0; i<N; i++)
     {
+        tmp = std::vector<double>(N,(T)0);
         for(int j=0; j<N; j++)
         {
-            tempRes = (T)0;
+            T tempRes = (T)0;
             for(int n=0; n<N; n++)
             {
-                tempRes += m1(i,n)*m2(n,j);
+                tempRes += mv(i,n)*m(n,j);
             }
-            result(i,j) = tempRes;
+            tmp[j] = tempRes;
+        }
+        //sor beszúrása mv i.sora helyére
+        for(int k=0; k<N; k++)
+        {
+            mv(i,k) = tmp[k];
         }
     }
-    return result;
+    return std::move(mv);
+}
+
+template<typename T>
+MatrixNxN<T>&& operator* (MatrixNxN<T> const& m, MatrixNxN<T>&& mv)
+{
+    if (!sizecheck(m,mv)) {sizerr();}
+    const int N = m.size();
+    std::vector<T> tmp;
+    //ideiglenes oszlop tmp, valamint eddigiektől eltérő iteráció, mert a jobboldali mátrix memóriája használható fel
+    for(int i=0; i<N; i++)
+    {
+        tmp = std::vector<double>(N,(T)0);
+        for(int j=0; j<N; j++)
+        {
+            T tempRes = (T)0;
+            for(int n=0; n<N; n++)
+            {
+                tempRes += m(j,n)*mv(n,i);
+            }
+            tmp[j] = tempRes;
+        }
+        //sor beszúrása mv i.oszlopa helyére
+        for(int k=0; k<N; k++)
+        {
+            mv(k,i) = tmp[k];
+        }
+    }
+    return std::move(mv);
+}
+
+template<typename T>
+MatrixNxN<T>&& operator* (MatrixNxN<T>&& mv, MatrixNxN<T>&& m)
+{
+    if (!sizecheck(m,mv)) {sizerr();}
+    const int N = m.size();
+    std::vector<T> tmp;
+    //ideiglenes sor tmp
+    for(int i=0; i<N; i++)
+    {
+        tmp = std::vector<double>(N,(T)0);
+        for(int j=0; j<N; j++)
+        {
+            T tempRes = (T)0;
+            for(int n=0; n<N; n++)
+            {
+                tempRes += mv(i,n)*m(n,j);
+            }
+            tmp[j] = tempRes;
+        }
+        //sor beszúrása mv i.sora helyére
+        for(int k=0; k<N; k++)
+        {
+            mv(i,k) = tmp[k];
+        }
+    }
+    return std::move(mv);
 }
 
 //transzponálás (2 verzió)
 template<typename T>
 MatrixNxN<T> transpose(MatrixNxN<T> const& m)
-{
-    MatrixNxN<T> result;
-    int N = m.sizeN;
-    result.data.resize(m.data.size());
-    result.sizeN = N;
-    for(int i=0; i<N; i++)
-    {
-        for(int j=0; j<N; j++)
-        {
-            if (i==j)
-            {
-                result(i,j) = m(i,j);
-            }
-            else
-            {
-                result(i,j) = m(j,i);
-            }            
-        }
-    }
-    // próbálkoztam for_each()-csel is, de onnét nem férek hozzá az indexekhez -> nem tudom, melyik m.data elemet kéne másoljam
-    return result;
+{    
+    const int N = m.sizeN;
+    return MatrixNxN<T>(lambda2{}, N, [&](int i, int j){return m(j,i);});
 }
 
 template<typename T>
 MatrixNxN<T>&& transpose(MatrixNxN<T>&& m)
 {
     //felső háromszögre meghívva -> ha csak 1x1-es a mátrix, nem fog működni -> ezt külön lekezelem
-    int N = m.sizeN;
+    const int N = m.sizeN;
     
     if(N == 1){return std::move(m);}
     
-    for(int i=1; i<N-1; i++)
+    for(int i=1; i<N; i++)
     {
         for(int j=0; j<i; j++)
         {
@@ -355,9 +442,9 @@ MatrixNxN<T>&& transpose(MatrixNxN<T>&& m)
 template<typename T>
 std::ostream& operator<< (std::ostream& o, MatrixNxN<T> const& m)
 {
-    int n = m.data.size();
-    int N = m.sizeN;
-    o << "size: " << N << "x" << N << std::endl;    
+    const int n = m.data.size();
+    const int N = m.sizeN;
+    o <<  N << "x" << N << std::endl;    
     if (n>0)
     {
         for (int i=1; i<n; i++)
