fd498b024f6805cba78a2b6fa8460308784481e7
onifizirib <48030636+onifizirib@users.noreply.github.com>
2019-04-23 02:39:32 +0200

    Update main.cpp

--- hf0320/main.cpp
+++ hf0320/main.cpp
@@ -3,22 +3,13 @@
 #include <iostream>
 
 int main(int, char**) {
-    std::cout << "Hello, world!\n";
-    std::cout<< "----------------------------------------------------\n";
     MatrixNxN<int> m;
-    std::cout << "initializing empty matrix: " <<m;
+    std::cout << "initializing empty matrix:\n" <<m;
     
     std::vector<int> v{1,11,111,2,22,222,3,33,333};
     MatrixNxN<int> m1(3, [=](int i){return v[i];});
-    std::cout <<"initializing matrix with a vector as its data: " <<m1;
-    
-    /*
-    MatrixNxN<int> m2(3, [=](int i){return v[i];});
-    m2 += m1;
-    //úgy néz ki, a matrix.begin() nem megfelelő. a pointereknek is különböző típusa van? mi a feloldás akkor, 
-    //ha nem tudom "kiköpetni" egy függvénnyel a matrix.data.begin()-t?
-    std::cout << m1+m2;
-    */
+    std::cout <<"initializing non-empty matrix:\n" <<m1;  
+    std::cout <<"-----------------------------------\n";
     
     return 0;
 }
