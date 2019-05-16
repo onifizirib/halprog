31e3241edd0f5f4497ccf93567f2d698a1db572a
onifizirib <48030636+onifizirib@users.noreply.github.com>
2019-05-16 02:33:14 +0200

    Add files via upload

---
+++ beadando/main.cpp
@@ -0,0 +1,192 @@
+#include <iostream>
+//#include <string>
+//#include <sstream>
+#include <fstream>
+#include "statistics.h"
+
+int main(int, char**) {
+{    
+    std::cout << "Hello, world!\n";
+    graph<int> proba;
+    int A = 9;
+    int B = 8;
+    int C = 4;
+    proba.addNode(A);
+    proba.addNode(B);
+    proba.addNode(B);
+    proba.addNode(C);
+    proba.addLink(A, B);
+    proba.addLink(A, B);
+    std::vector<std::vector<int>> const v = proba.getAdjList();
+    std::cout<< "adjList hossza: " << v.size() << ", NodeNum: " << proba.getNodeNum() <<"\n";
+    std::cout<< "adjList elemeinek hossza: ";
+    for(int i=0; i<v.size(); i++)
+    {
+        std::cout << v[i].size() << ", ";
+    }
+    std::cout << "linkNum: " << proba.getLinkNum() <<"\n";
+    //std::cout<<v[0][0]<<", "<<v[0][1]<<"\n";
+    //std::cout<<v[1][0]<<", "<<v[1][1]<<"\n";
+    std::cout << proba;
+
+    std::cout << "link deletion then node deletion\n";
+    proba.deleteLink(B,A);
+    proba.deleteNode(B);    
+    proba.deleteLink(C,A);
+    proba.deleteNode(B);
+    std::cout << proba;
+
+//------------------
+    graph<int> proba1 = proba;
+    proba1 = std::move(proba);
+}
+//------------------
+{
+    std::cout << "-----------------\n";
+    graph<int> proba2(10);
+    proba2.addLink(0,3);
+    proba2.addLink(6,3);
+    proba2.addLink(7,3);
+    proba2.addLink(2,5);
+    proba2.addLink(5,3);
+    proba2.addLink(2,3);
+    std::cout << proba2;
+    std::vector<std::vector<int>> const wat = proba2.getAdjList();
+
+//------------------
+
+    std::cout << "---distr test---\n";
+    int binNum = 3;
+    distr D(0.0, 6.0, binNum);
+    std::cout<<"binnum="<<D.binNum <<", binsize=" <<D.binsize << ", num of intervals=" << D.hist.size() <<"\n";
+    // különben valamiért double(5)-ként értelmezi, és a binSize-os konstruktort hívja meg..
+    D.feltolt(proba2);
+    std::cout << "-----------------\n" <<D;
+
+    std::cout << "---clustering coefficient test---\n";
+    std::vector<double> clusterC;
+    clusterC.resize(proba2.getNodeNum());
+    int i = 0;
+    std::for_each(clusterC.begin(), clusterC.end(), [&](double& coeff)
+                {
+                    coeff = clusCoeff(wat[i][0], proba2);
+                    std::cout << "c" << i << " = " << coeff << std::endl;
+                    i++;                    
+                });
+    double avgC = avgClusCoeff(proba2);
+    std::cout << "avg clustering coeff = " << avgC << "\n";
+    std::cout << "---------\n";
+    /*
+    //test for findNeighbors and commonNodes
+    std::vector<int> nei3 = findNeighbors(wat[3][0],proba2);
+    //std::for_each(nei3.begin(), nei3.end(), [&](int const& node){ std::cout << node << std::endl;});
+    std::cout << "wat[3][0]: " << wat[3][0] << " ->nei3.size: " << nei3.size() << "\n";
+    for(int i=0; i<nei3.size();i++){std::cout<<nei3[i]<<" ";}
+
+    std::vector<int> nei5 = findNeighbors(wat[5][0],proba2);
+    std::cout << "\nwat[5][0]: " << wat[5][0] << " ->nei5.size: " << nei5.size() << "\n";
+    for(int i=0; i<nei5.size();i++){std::cout<<nei5[i]<<" ";}
+
+    std::vector<int> com35 = commonNodes(nei3, nei5);
+    std::cout << "\ncommon element of nei3 and nei5:\n";
+    for(int i = 0; i< com35.size(); i++){std::cout << com35[i] << " ";}
+    */
+
+   std::cout << "---avgPathLen test---\n";
+   std::cout << "for node 3:\n" << avgPathLenFromNode(wat[3][0], proba2) << "\n";
+   proba2.addLink(1,5);
+   proba2.addLink(4,1);
+   proba2.addLink(4,8);
+   proba2.addLink(9,7);
+   std::cout << "after adding links, pathLen for node 3:\n" << avgPathLenFromNode(3, proba2) << "\n";
+   proba2.addLink(0,4);
+   std::cout << "after adding links, pathLen for node 3:\n" << avgPathLenFromNode(3, proba2) << "\n";
+
+   std::cout << "for the proba2 graph: " << avgPathLen(proba2) << "\n";
+   for(int i=0; i<wat.size(); i++)
+   {
+       std::cout<<i<<": " <<avgPathLenFromNode(i, proba2) << std::endl;
+   }
+   std::cout << "-----avg clustering coeff test-----\n";
+   std::cout << "for node 3:\n" << clusCoeff(wat[3][0], proba2) << "\n";
+   std::cout << "for the proba2 graph: " << avgClusCoeff(proba2) << "\n";
+   std::cout << "---------\n";
+}
+//-------------------------------------------------------------------
+{
+   std::cout << "----Erdos-Renyi test----\n";
+   int nodenum = 200;
+   double linkprob = 0.3;
+   graph<int> erdos = ER(nodenum, linkprob);
+   //std::cout << erdos;
+   statistics erdos_gen(erdos);
+   statistics erdos_exp(nodenum, linkprob);
+   //itt remélem hogy nem a BA model predikciót hívja meg, mert szeret konvertálgatni int-et double-lá -.-
+
+   std::vector<double> differOfDist;
+   differOfDist.resize(nodenum);
+   //std::cout<<"elementwise abs difference of degree distribution: ";
+   for(int i=0; i<nodenum; i++)
+   {
+       differOfDist[i] = (std::abs(erdos_gen.degreeDist.hist[i]-erdos_exp.degreeDist.hist[i]));
+       //std::cout << differOfDist[i] <<", ";
+   }
+   //std::cout<<std::endl;
+   std::cout<<"sum of elementwise abs difference between degree dist-s: "<<std::accumulate(differOfDist.begin(), differOfDist.end(), 0.0)<<std::endl;
+   
+   std::cout << "expected clustering coefficient = " << erdos_exp.avgClus << std::endl;
+   std::cout << "   difference of average clustering coefficient: "<< std::abs(erdos_gen.avgClus - erdos_exp.avgClus) << "\n";
+   std::cout << "expected average shortest path = " << erdos_exp.avgPath << std::endl;
+   std::cout << "   difference of average shortest path: "<< std::abs(erdos_gen.avgPath - erdos_exp.avgPath) << "\n";
+   std::cout << "expected number of links = " << erdos_exp.linkNum << std::endl;
+   std::cout << "   difference of link number: "<< std::abs(erdos_gen.linkNum - erdos_exp.linkNum) << "\n";
+   std::cout << "expected average degree = " << erdos_exp.avgDegr << std::endl;
+   std::cout << "   difference of average degree: "<< std::abs(erdos_gen.avgDegr - erdos_exp.avgDegr) << "\n";
+   std::cout << "----------------------\n";
+
+   //statistics kiiratása
+   std::ofstream outfile;
+   outfile.open("erdos.txt");
+   if(outfile.is_open())
+   {
+       outfile << erdos_gen;
+       outfile << erdos_exp;
+       outfile.close();
+   }
+   else{ std::cout << "Could not open output file.\n"; }
+}
+
+//-------------------------------------------------------------------
+{
+   std::cout << "----Watts-Strogatz test----\n";
+   int q = 3;
+   double beta = 0.0;
+   graph<int> watts = WS(9, q, beta);
+   std::cout<<watts; 
+   
+   watts = WS(61, q, beta);    
+   std::cout<<"WS(61,q,beta): avg path length from node0 = "<<avgPathLenFromNode(0,watts)<<"\n";
+   watts = WS(62, q, beta);
+   std::cout<<"WS(62,q,beta): avg path length from node0 = "<<avgPathLenFromNode(0,watts)<<"\n";
+   
+   int nodenum = 100;
+   std::cout<<"WS(100,q,beta) test\n";
+   watts = WS(nodenum, q, beta);
+   statistics watts_gen(watts);
+   statistics watts_exp(nodenum, q, beta);
+   
+   //std::cout<<std::endl;
+   std::cout<<"there was no expectation for the degree distribution."<<std::endl;
+   
+   std::cout << "expected clustering coefficient = " << watts_exp.avgClus << std::endl;
+   std::cout << "   difference of average clustering coefficient: "<< std::abs(watts_gen.avgClus - watts_exp.avgClus) << "\n";
+   std::cout << "expected average shortest path = " << watts_exp.avgPath << std::endl;
+   std::cout << "   difference of average shortest path: "<< std::abs(watts_gen.avgPath - watts_exp.avgPath) << "\n";
+   std::cout << "expected number of links = " << watts_exp.linkNum << std::endl;
+   std::cout << "   difference of link number: "<< std::abs(watts_gen.linkNum - watts_exp.linkNum) << "\n";
+   std::cout << "expected average degree = " << watts_exp.avgDegr << std::endl;
+   std::cout << "   difference of average degree: "<< std::abs(watts_gen.avgDegr - watts_exp.avgDegr) << "\n";
+   std::cout << "----------------------\n";
+}
+
+}
---
+++ beadando/statistics.h
@@ -0,0 +1,178 @@
+#include "distr.h"
+
+struct statistics
+{
+    //egy gráf vagy elméleti modell alapján tölthető fel
+    int nodeNum;        //number of nodes
+    int linkNum;        //numebr of links
+    double avgPath;     //average shortest path
+    double avgClus;     //average clustering coefficient
+    double avgDegr;     //average degree
+    distr degreeDist;   //degree distribution
+
+//konstruktorok
+    statistics()
+    {
+        nodeNum=0, linkNum=0, avgPath=0.0, avgClus=0.0, avgDegr=0.0;
+        //distr-nek van default konstruktora
+    }
+    statistics(statistics const& cpy) = default;
+    /* TODO
+    statistics(statistics && mv)
+    */
+
+//expected statistisc to ER graph with parameter nodenum and linkprob
+   statistics(int const& nodenum, double const& linkprob)
+   {
+       if(nodenum < 2)
+       {
+           std::cout << "zero or one noded estimation is given! (dividing with zero or log0 is performed)\n";
+           statistics();
+       }
+       else
+       {       
+           nodeNum = nodenum;
+           double dlN = double(nodenum);
+
+           linkNum = linkprob*dlN*(dlN-1.0)/2.0;
+           avgDegr = dlN*linkprob;
+           //igazából pontosan (dlN-1)linkprob
+           double euler_gamma = 0.57721566490153286060651209008240243104215933593992;
+           //form https://hu.wikipedia.org/wiki/Euler%E2%80%93Mascheroni-%C3%A1lland%C3%B3
+           avgPath = (log(dlN)-euler_gamma)/log(avgDegr) + 0.5;
+           avgClus = linkprob;
+
+           degreeDist = distr(0.0, dlN, nodenum);
+           std::random_device rd;
+           std::minstd_rand rng(rd());
+           std::binomial_distribution<int> binom(nodenum-1, linkprob);
+           for(int i=0; i<10000; i++)
+           {
+               degreeDist.hist[binom(rng)]++;
+           }
+           std::for_each(degreeDist.hist.begin(), degreeDist.hist.end(), [](double& value){value /= 10000.0; }); 
+       }      
+   }
+
+//expected statistisc to WS graph with parameter nodenum, "distance of farest neighbour to connect" q, and rewiring probability beta
+   statistics(int const& nodenum, int q, int const& beta)
+   {
+       if(nodenum < 2)
+       {
+           std::cout << "zero or one noded estimation is given! (dividing with zero or log0 is performed)\n";
+           statistics();
+       }
+       else
+       {
+           int thres = floor((double)nodenum/2.0);
+           if (q > thres) {q = thres;}
+
+           nodeNum = nodenum;
+           double dlN = double(nodenum);
+           double dlq = double(q);
+
+           linkNum = 2*q;
+           avgDegr = 2.0*dlq;
+           if (beta == 0)
+           {
+               avgPath = dlN/(4.0*dlq);
+               avgClus = (3.0*dlq-3.0)/(4.0*dlq-2.0);
+           }
+           else if (beta == 1)
+           {
+               avgPath = log(dlN)/log(2.0*dlq);
+               avgClus = (2.0*dlq)/(dlN-1.0);
+           }
+           else
+           {
+               std::cout << "beta value is not 0 nor 1, thus some quantities cannot be predicted.\n";
+               avgPath = -1.0;
+               avgClus = -1.0;
+           }
+
+           degreeDist = distr();
+           //no prediction available for this quantity
+        }
+   }
+
+//expected statistisc to BA graph with parameter timesteps t, number of links a new node comes with m
+   statistics(int const& m, int const& t)
+   {
+       nodeNum = t;
+       double dlN = (double)nodeNum;
+       double dlm = (double)m;
+       linkNum = m*t;
+       avgDegr = 2*m;
+       avgClus = dlm*std::pow((log(dlN)),2.0)/(8.0*dlN);
+
+       degreeDist = distr(dlm, dlm-1.0+(double)t, t-1); 
+       //legalábbis gráf esetére ez a tartomány biztos, így az elméleti legenerálást is ide korlátozom
+       for(int i=0; i<t-1; i++)
+       {
+           degreeDist.hist[i] = 2.0*std::pow(dlm, 2.0)*std::pow((double)i, -3.0);
+       }
+       double sum = std::accumulate(degreeDist.hist.begin(), degreeDist.hist.end(), 0.0);
+       std::for_each(degreeDist.hist.begin(), degreeDist.hist.end(), [&](double& value){value /= sum; });
+       //és így normálva lett az hist elemeinek összege egyre (amit egy gráf produkálhat arra biztosan fennáll ez a tulajdonság)
+
+       double euler_gamma = 0.57721566490153286060651209008240243104215933593992;
+       avgPath = (log(dlN) - log(dlm/2.0) - 1.0 - euler_gamma) / ((log(log(dlN))) + log(dlm+2.0)) + 1.5;
+       /*
+       with this non-tunable model the scalefree-exponent is 3, therefore the <l> expected value is
+       (lnN - ln(m/2) - 1 - euler_gamma) / (lnlnN + ln(m+2)) + 3/2
+       according to this paper: http://www.if.pw.edu.pl/~jholyst/data/PhysRevE_70_056110.pdf
+       */
+   }
+
+//mennyiségek számolása a kész gráf alapján
+   template<typename T>
+   statistics(graph<T> const& graf)
+   {    
+        nodeNum = graf.getNodeNum();
+        linkNum = graf.getLinkNum();
+        avgDegr = graf.avgDegree();
+        avgPath = avgPathLen(graf);
+        avgClus = avgClusCoeff(graf);
+        degreeDist = distr(0.0, (double)nodeNum, nodeNum);
+        degreeDist.feltolt(graf);
+   }  
+
+//metódusok
+   template<typename T>
+   void update(graph<T> const& graf)
+   {
+        nodeNum = graf.getNodeNum();
+        linkNum = graf.getLinkNum();
+        avgDegr = graf.avgDegree();
+        avgPath = avgPathLen(graf);
+        avgClus = avgClusCoeff(graf);
+        degreeDist = distr(0.0, (double)nodeNum, nodeNum);
+        degreeDist.feltolt(graf);
+   }
+};
+
+//kiírás
+std::ostream& operator<< (std::ostream& o, statistics const& stat)
+{
+    o << stat.nodeNum << std::endl;
+    o << stat.linkNum << std::endl;
+    o << stat.avgDegr << std::endl;
+    o << stat.avgPath << std::endl;
+    o << stat.avgClus << std::endl;
+
+    o << stat.degreeDist.mini << " ";
+    o << stat.degreeDist.maxi << " ";
+    o << stat.degreeDist.binNum << " ";
+    o << stat.degreeDist.binsize << std::endl;
+
+	int n = stat.degreeDist.hist.size();
+	if( n > 0 )
+	{
+		for(int i=0; i<n-1; ++i)
+		{
+			o << stat.degreeDist.hist[i] << " ";
+		}
+		o << stat.degreeDist.hist[n-1] << std::endl;
+	}
+	return o;
+}
