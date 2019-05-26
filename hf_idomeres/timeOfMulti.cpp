5fd509848c7591e396441c069f8406926ffd276a
onifizirib <48030636+onifizirib@users.noreply.github.com>
2019-05-26 02:16:50 +0200

    Update timeOfMulti.cpp

--- hf_idomeres/timeOfMulti.cpp
+++ hf_idomeres/timeOfMulti.cpp
@@ -6,27 +6,21 @@
 #include <iomanip>
 
 //időméréshez
-double minOfTrials(int const& numOfTrials, int const& N)
+double minOfTrials(int const& numOfTrials, int const& N, 
+					std::random_device & rd, std::minstd_rand & gen, 
+					std::uniform_real_distribution<double> & unif)
 {
     //random mátrixok előállítása
-    std::random_device rd{};
-    std::minstd_rand gen(rd());
-    std::uniform_real_distribution<double> unif(0, 100);
-    MatrixNxN<double> m1(N, [&](int i){return unif(gen);});
-    MatrixNxN<double> m2(N, [&](int i){return unif(gen);});
-
-    //időmérés előkészítése (mikroszekundumban)
-    auto t_begin = std::chrono::high_resolution_clock::now();
-    auto t_end = std::chrono::high_resolution_clock::now();
-        //sajnos array-t nem tudok létrehozni, mert a bemenettől függ, hogy neki mekkora a hossza.
     std::vector<double> trialTimes(numOfTrials);
 
     //időmérés numOfTrial-szor:
     for(int i=0; i < numOfTrials; i++)
     {
-        t_begin = std::chrono::high_resolution_clock::now();
+		MatrixNxN<double> m1(N, [&](int i){return unif(gen);});
+    	MatrixNxN<double> m2(N, [&](int i){return unif(gen);});
+        auto t_begin = std::chrono::high_resolution_clock::now();
         MatrixNxN<double> m = m1*m2;
-        t_end = std::chrono::high_resolution_clock::now();
+        auto t_end = std::chrono::high_resolution_clock::now();
         trialTimes[i]= (static_cast<std::chrono::duration<double, std::micro>>(t_end-t_begin)).count();
     }
     
@@ -35,27 +29,37 @@ double minOfTrials(int const& numOfTrials, int const& N)
 
 int main() {
 
-    std::vector<int> sizeNs;
+    std::random_device rd{};
+    std::minstd_rand gen(rd());
+    std::uniform_real_distribution<double> unif(0, 100);
+
+	std::vector<int> sizeNs;
     std::vector<double> mulTimes32;
 	std::vector<double> mulTimes1;
-    for(int i=2; i < 515; i+=4) 
+    for(int i=2; i < 515; i+=4)
 	{
         sizeNs.push_back(i);
-        mulTimes32.push_back(minOfTrials(32, i));
-		mulTimes1.push_back(minOfTrials(1, i));
+        mulTimes32.push_back(minOfTrials(32, i, rd, gen, unif));
+		mulTimes1.push_back(minOfTrials(1, i, rd, gen, unif));
     }
     unsigned int n = mulTimes32.size();
 	std::ofstream output32;
 	std::ofstream output1;
   	output32.open("timesOfMulti32.txt");
 	output1.open("timesOfMulti1.txt");
+	if(!output32.is_open() || !output1.is_open())
+	{
+		std::cout << "sikertelen fajliras!\n";
+		return -1;
+	}
+	std::cout << std::setprecision(16);
   	for(unsigned int i=0; i<n; i++)
     {
-        output32 << sizeNs[i] << " " << std::setprecision(16) << mulTimes32[i] << std::endl;
-		output1 << sizeNs[i] << " " << std::setprecision(16) << mulTimes1[i] << std::endl;
+        output32 << sizeNs[i] << " " << mulTimes32[i] << std::endl;
+		output1  << sizeNs[i] << " " << mulTimes1[i]  << std::endl;
     }  
 	output32.close();
 	output1.close();
-
+    
     return 0;
 }
